79925d2c4a0facf2fe6a2e5552cdd1398c75ba6f
Ashan Millewa <ashanmillewa@gmail.com>
2024-03-22 10:37:49 +0530

    create footer

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.css'
+
+const footer = () => {
+  return (
+    <footer className='footer'>
+        &#169; Copyright &nbsp; <strong> DTS.</strong> &nbsp;  All Rights Reserved
+Designed by &nbsp;  <a href="https://www.dockyardsolutions.lk/" className='footerlink'> Dockyard Total Solutions. </a>
+    </footer>
+  );
+}
+
+export default footer
